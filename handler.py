6a7fa1c8fadf1a4bd1cc66208c2bf0a8bff94695
Andrés Reverón Molina <andressser@gmail.com>
2020-07-02 09:47:53 +0200

    initial commit

---
+++ handler.py
@@ -0,0 +1,77 @@
+import json
+import logging
+import os
+
+from telegram import Update, Bot
+from telegram.ext import CommandHandler, Dispatcher, MessageHandler, Filters
+
+# Enable logging
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+# Define responses
+OK_RESPONSE = {
+    'statusCode': 200,
+    'headers': {'Content-Type': 'application/json'},
+    'body': json.dumps('ok')
+}
+ERROR_RESPONSE = {
+    'statusCode': 400,
+    'body': json.dumps('Oops, something went wrong!')
+
+    
+def configure_telegram():
+    """
+    Configures the bot with a Telegram Token.
+    Returns a bot instance.
+    """
+
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    if not TELEGRAM_TOKEN:
+        logger.error('The TELEGRAM_TOKEN must be set')
+        raise NotImplementedError
+
+    return Bot(TELEGRAM_TOKEN)
+
+
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
+
+
+def help_command(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+# Initialize bot and dispatcher to register handlers
+bot = configure_telegram()
+dp = Dispatcher(bot, None, use_context=True)
+# on noncommand i.e message - echo the message on Telegram
+dp.add_handler(MessageHandler(Filters.text, echo))
+# on different commands - answer in Telegram
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("help", help_command))
+
+
+def process_update(event, context):
+    """
+    Processes the received update and sends it to the dispatcher.
+    """
+    logger.info(f'Event: {event}')
+
+    try:
+        dp.process_update(Update.de_json(json.loads(event.get("body")), bot))
+    except Exception as e:
+        logger.error(e)
+        return ERROR_RESPONSE
+
+    return OK_RESPONSE
